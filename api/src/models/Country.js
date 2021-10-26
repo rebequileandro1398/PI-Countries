81e7aeb7264c32283efd7f1ea38445a3b8f09eac
rebequileandro1398 <rebequileandro@gmail.com>
2021-10-26 12:32:29 -0300

    models

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,44 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+
+    id: {
+      type: DataTypes.CHAR(3),
+      allowNull: false,
+      primaryKey: true
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    continents: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    subregion: {
+      type: DataTypes.STRING
+    },
+
+    area: {
+      type: DataTypes.REAL
+    },
+
+    population: {
+      type: DataTypes.INTEGER
+    }
+
   });
 };
